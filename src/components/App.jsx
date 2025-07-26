564ba9dc45074fc76af21d37f1c0cf5e2daa2136
Pizzaman333 <andrijmalchevv369@gmail.com>
2025-07-27 02:57:37 +0300

    everything

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import user from '../data/user.json';
+import { Profile } from './profile/Profile';
+import stats from '../data/stats.json';
+import { Statistics } from './statistics/Statistics';
+import friends from '../data/friends.json';
+import { Friends } from './friendslist/FriendsList';
+import transactions from '../data/transactions.json';
+import { TransactionsHistory } from './transactionshistory/TransactionsHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="A unique title!" stats={stats}/>
+      <Statistics stats={stats}/>
+      <Friends friends={friends}/>
+      <TransactionsHistory transactions={transactions} />
+    </>
   );
 };
